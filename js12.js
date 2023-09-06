de5797166b89d582b39ccb13755dbcc9f0545f41
shinyeeun789 <tlsdpdms789@naver.com>
2023-09-06 16:15:12 +0900

    readFile을 사용하여 텍스트 파일 읽어오기

--- js12.js
+++ js12.js
@@ -1,5 +1,11 @@
 const fs = require("fs");
 
-fs.readFile('./shin.txt', 'utf-8', function(err, data) {
-    console.log(data);                                                       
+// 비동기 방식
+fs.readFile('./shin.txt', 'utf8', function(err, data) {
+    console.log(data);
+    if(err === undefined) {
+        console.log(err);
+    } else {
+        console.log(data);
+    }
 });
