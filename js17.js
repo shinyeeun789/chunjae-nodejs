b88a6eedebe675161a46ba63e42272bb7b27807b
shinyeeun789 <tlsdpdms789@naver.com>
2023-09-07 16:31:25 +0900

    URL 모듈을 사용 (GET 방식)

---
+++ js17.js
@@ -0,0 +1,32 @@
+// URL 모듈 사용법
+const http = require("http");
+const { tmpdir } = require("os");
+const url = require("url");
+
+const app = http.createServer((request, response) => {
+    response.writeHead(200, {'Content-Type' : 'text/html; charset=UTF-8'});
+    var queryData = url.parse(request.url, true).query;             // id:LEE -> queryData.id
+    var tmp1 = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> 메인 페이지 </title>
+    </head>
+    <body>
+        <nav>
+            <ul>
+                <li><a href="/?id=LEE">LEE</a></li>
+                <li><a href="/?id=PARK">PARK</a></li>
+                <li><a href="/?id=KIM">KIM</a></li>
+            </ul>
+        </nav>
+        <div class="container">
+            <h2>${queryData.id}</h2>
+        </div>
+    </body>
+    </html>`;
+    response.end(tmp1);
+});
+
+app.listen(3000);
